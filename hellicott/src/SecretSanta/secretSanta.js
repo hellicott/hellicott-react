a2b926798e2275100941b21561e94ed7f7815521
HEllicott <hannah.ellicott95@gmail.com>
2021-12-04 17:17:37 +0000

    removed unused stuff

--- hellicott/src/SecretSanta/secretSanta.js
+++ hellicott/src/SecretSanta/secretSanta.js
@@ -5,7 +5,6 @@ import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import InputSecretSantas from './inputSecretSantas';
 
 const steps = ['Enter participants', 'Confirm', 'Send secret santa emails!'];
 
@@ -23,12 +22,6 @@ export default function SecretSanta() {
   const handleReset = () => {
     setActiveStep(0);
   };
-
-  const getStep = (step) => {
-        if (step === 0) {
-            return <InputSecretSantas />
-        }
-  }
 
   return ( 
     <Box sx={{ width: '80%' , padding: 8}}>
