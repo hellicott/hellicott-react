5c56e3ec7e92e3f7b832cce8aedfbf5fa845155f
HEllicott <hannah.ellicott95@gmail.com>
2022-10-04 22:53:25 +0100

    added player selection for rps

--- hellicott/src/rockPaperScissors.js
+++ hellicott/src/rockPaperScissors.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faQuestion } from '@fortawesome/free-solid-svg-icons'
 import { faHandBackFist } from '@fortawesome/free-solid-svg-icons'
@@ -9,38 +9,59 @@ import { faHandScissors } from '@fortawesome/free-solid-svg-icons'
 
 
 function RockPaperScissors() {
+  const [botChoice, setBotChoice] = useState(faQuestion);
+  const [playerChoice, setPlayerChoice] = useState(faQuestion);
 
   return (
     <div id="rockpaperscissors" className="App">
-      <header className="App-section darker-section" style={{backgroundColor: '#282c34'}}>
+      <header className="App-section darker-section">
         <p>Rock Paper Scissors!</p>
         <div className="row">
           <div className="column darker-section">
             <p>Bot</p>
-            <FontAwesomeIcon icon={faQuestion} />
+            <FontAwesomeIcon icon={botChoice} />
           </div>
           <div className="column lighter-section">
             <p>Player</p>
-            <FontAwesomeIcon icon={faQuestion} />
+            <FontAwesomeIcon icon={playerChoice} />
             <p>
-              <button className='subtle-button' onClick={chooseBotAnswer}><FontAwesomeIcon icon={faHandBackFist} /></button>
-              <button className='subtle-button' onClick={chooseBotAnswer}><FontAwesomeIcon icon={faHand} /></button>
-              <button className='subtle-button' onClick={chooseBotAnswer}><FontAwesomeIcon icon={faHandScissors} /></button>
+              <button className='subtle-button' onClick={updatePlayerRock}><FontAwesomeIcon icon={faHandBackFist} /></button>
+              <button className='subtle-button' onClick={updatePlayerPaper}><FontAwesomeIcon icon={faHand} /></button>
+              <button className='subtle-button' onClick={updatePlayerScissors}><FontAwesomeIcon icon={faHandScissors} /></button>
             </p>
           </div>
         </div>
+        <p id='rpsResult'>Who will win?</p>
       </header>
     </div>
   );
 
-  function chooseBotAnswer(e) {
-    e.preventDefault();
-
-    const randomNum = "#" + Math.floor(Math.random()*3);
-
-    console.log(randomNum);
-
+  function updatePlayerRock() {
+    setPlayerChoice(faHandBackFist);
   }
+  function updatePlayerPaper() {
+    setPlayerChoice(faHand);
+  }
+  function updatePlayerScissors() {
+    setPlayerChoice(faHandScissors);
+  }
+  function getSymbol(num) {
+    if (num === 0){
+      console.log(num);
+      return {faHandBackFist}.faHandBackFist;
+    }
+    if (num === 1){
+      console.log(num);
+
+      return faHand;
+    }
+    if (num === 2){      
+      console.log(num);
+
+      return faHandScissors;
+    }
+  }
+
 
 }
 
