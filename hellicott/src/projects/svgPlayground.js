a83c5585773eed06a3f00a2284a76dc19aef6486
HEllicott <hannah.ellicott95@gmail.com>
2022-11-11 22:40:27 +0000

    working svg colour editing

--- hellicott/src/projects/svgPlayground.js
+++ hellicott/src/projects/svgPlayground.js
@@ -3,24 +3,33 @@ import React, { useState } from 'react';
 import Mountains from '..\\svgs\\mountain.js'
 
 function SvgPlayground() {
-    const [red, setRed] = useState("50");
-
+    const [mountainColour, setMountainColour] = useState("#00ff00");
+    const [moonColour, setMoonColour] = useState("#ffff00");
 
     return (
         <div id="svgPlayground" className="App">
+            <form onSubmit={handleColourChange}>
+                <label>
+                    Mountain Hex Colour
+                    <input type="text" name="mountainHex" />
+                </label>
+                <label>
+                    Circle Hex Colour
+                    <input type="text" name="moonHex" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
             <div>
-                <input type="range" min="0" max="255" id="redRange" onChange={handleSlider}/>
-            </div>
-            <div>
-              <Mountains moon="#00ffff" mountains="#ffff00" />
+                <Mountains moon={moonColour} mountains={mountainColour} />
             </div>
         </div>
       );
 
-    function handleSlider(e) {
-        setRed(e.target.value);
-        console.log(red);
-    }
+      function handleColourChange(event) {
+        event.preventDefault();
+        setMountainColour(event.target.mountainHex.value);
+        setMoonColour(event.target.moonHex.value);
+      }
 
 }
 
--- hellicott/src/svgs/mountain.js
+++ hellicott/src/svgs/mountain.js
@@ -1,6 +1,6 @@
 function Mountains(props) {
     return (
-        <svg width="210mm" height="297mm" version="1.1" viewBox="0 0 210 297" xmlns="http://www.w3.org/2000/svg">
+        <svg width="167.63mm" height="118.3mm" version="1.1" viewBox="23 45 167.63 118.3" xmlns="http://www.w3.org/2000/svg">
             <circle cx="104.86" cy="98.939" r="50" fill={props.moon}/>
             <path 
                 d="m22.876 166.84 167.63 0.39442c-62.595-122.22-53.236-117.29-91.9-33.92-44.441-47.219-20.931-38.575-75.729 33.526z" 
