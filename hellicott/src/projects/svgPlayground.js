b0e10a80b701946d6e946919d1a69df11d02200e
HEllicott <hannah.ellicott95@gmail.com>
2022-11-12 04:11:21 +0000

    tidied layout

--- hellicott/src/projects/svgPlayground.js
+++ hellicott/src/projects/svgPlayground.js
@@ -5,20 +5,23 @@ import { CompactPicker } from 'react-color';
 
 
 function SvgPlayground() {
-    // const [background, setBackground] = useState("#9199c1");
     const [background, setBackground] = useState({hex: "#9199c1"});
-
     const handleBackgroundChange = (background) => setBackground(background);
+    
     const [foreground, setForeground] = useState({hex: "#5e5e74"});
     const handleForegroundChange = (foreground) => setForeground(foreground);
 
     return (
         <div id="svgPlayground" className="App">
-            <div>
-                <p>Mountains</p>
-                <CompactPicker color={foreground} onChangeComplete={handleForegroundChange} />
-                <p>Circle</p>
-                <CompactPicker color={background} onChangeComplete={handleBackgroundChange} />
+            <div className="row">
+                <div className="column">
+                    <p>Mountains</p>
+                    <CompactPicker color={foreground} onChangeComplete={handleForegroundChange} />
+                </div>
+                <div className="column">
+                    <p>Circle</p>
+                    <CompactPicker color={background} onChangeComplete={handleBackgroundChange} />
+                </div>
             </div>
             <div>
                 <Mountains moon={background.hex} mountains={foreground.hex} />
